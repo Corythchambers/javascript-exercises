72bc3cc5d40ed40c5cf3b8ca7d3b228da5a05e21
corythchambers <corythchambers@gmail.com>
2024-05-25 19:44:23 -0700

    Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(arg1, arg2) {
+  return arg1 + arg2
 };
 
-const subtract = function() {
-	
+const subtract = function(arg1, arg2) {
+  return arg1 - arg2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, num) => total + num, 0);
+}
+
+const multiply = function(array) {
+  return array.reduce((total, num) => total * num, 1);
 };
 
-const multiply = function() {
-
+const power = function(val1, val2) {
+  return Math.pow(val1, val2);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(val) {
+  if (val === 0) return 1;
+  let total = 1;
+  for (let i = 1; i <= val; i++) {
+    total *= i;
+  }
+	return total;
 };
 
 // Do not edit below this line
