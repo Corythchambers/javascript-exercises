3706443e201bbb13a070b47d41d4fbd132942fc5
corythchambers <corythchambers@gmail.com>
2024-05-25 20:22:02 -0700

    Exercise 10, fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    let newNum = parseInt(num);
+
+    if (newNum < 0) return "OOPS";
+    if (newNum === 0) return 0;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= newNum; i++) {
+        let currentNumber = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = currentNumber;
+    }
+
+    return firstPrev;
 };
 
 // Do not edit below this line
